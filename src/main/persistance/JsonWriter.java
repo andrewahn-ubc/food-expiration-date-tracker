849e945275287ce1a84a80380ccbacf899e7da0e
Andrew Ahn <andrewahn21@gmail.com>
2023-03-08 12:06:44 -0800

    Fixed an outdated line of code in the JsonWriter class that was causing a compiler error.

--- src/main/persistance/JsonWriter.java
+++ src/main/persistance/JsonWriter.java
@@ -7,9 +7,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import static com.sun.tools.javac.util.LayoutCharacters.TAB;
-
-
 // Represents the object that transcribes the current fridge onto a file as a JSON object.
 public class JsonWriter {
     private final String destination;
@@ -30,7 +27,7 @@ public class JsonWriter {
     // EFFECTS: Adapts a fridge's information onto the destination file.
     public void write(Fridge fridge) {
         JSONObject jsonFridge = fridge.toJson();
-        String stringFridge = jsonFridge.toString(TAB);
+        String stringFridge = jsonFridge.toString(1);
         writer.print(stringFridge);
     }
 
